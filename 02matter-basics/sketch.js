404c35b8c27f64402233718b21de6d80749ea1fb
migueladn <migueladan.devel@gmail.com>
2018-01-05 06:37:46 +0000

    matter.js

---
+++ 02matter-basics/sketch.js
@@ -0,0 +1,42 @@
+
+//module aliases
+
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var boxes=[];
+
+var ground;
+
+function setup() {
+  createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  var options = {
+    isStatic: true
+  }
+  ground = Bodies.rectangle(200,height -50,width,10, options);
+  World.add(world,ground);
+}
+
+function mouseDragged() {
+  boxes.push(new Box(mouseX, mouseY, 20, 20));
+}
+
+function draw() {
+  background(51);
+  fill(255);
+
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+  noStroke();
+  fill(170);
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,width,10);
+}
