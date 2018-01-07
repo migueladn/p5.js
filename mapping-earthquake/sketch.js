b4046e36832715ba534494cdc8a473a09c785ff3
migueladn <migueladan.devel@gmail.com>
2018-01-07 04:59:05 +0000

    mapping earthquakes

--- mapping-earthquake/sketch.js
+++ mapping-earthquake/sketch.js
@@ -10,9 +10,12 @@ var lat = 49.2827;
 var lon = -123.1207;
 
 var zoom = 1;
+var earthquakes;
 
 function preload() {
     mapimage = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0,0/1024x512?access_token='+accessToken)
+    //earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
+    earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
 
 
@@ -37,13 +40,29 @@ function setup() {
   imageMode(CENTER);
   image(mapimage,0,0);
 
-  var cx = mercX(clon);
-  var cy = mercY(clat);
+  for (var i = 0; i < earthquakes.length;i++) {
+    var data = earthquakes[i].split(/,/);
 
-  var x = mercX(lon) - cx;
-  var y = mercY(lat) -cy;
-  console.log(x, y);
-  fill(255,0,255,200);
-  ellipse(x,y,8,8);
+    var lat = data[1];
+    var lon = data[2];
+
+    var mag = data[4];
+
+    mag = pow(10, mag)
+    mag = sqrt(mag);
+    var magmax = sqrt(pow(10,10));
+    stroke(255,0,255);
+    var d = map(mag, 0 , magmax , 0, 180);
+    var cx = mercX(clon);
+    var cy = mercY(clat);
+
+    var x = mercX(lon) - cx;
+    var y = mercY(lat) -cy;
+
+    fill(255,0,255,200);
+    ellipse(x,y,d,d);
+
+  }
+
 
 }
